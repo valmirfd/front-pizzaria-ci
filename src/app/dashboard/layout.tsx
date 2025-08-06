4eabc9d877617c2014ac9b4dc40499b31298bf95
Valmir Dornas <fernandocursos2021@gmail.com>
2025-08-06 17:32:53 -0300

    Componente Header

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,13 @@
+import { Header } from "./components/header";
+
+
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+
+    return (
+        <>
+            <Header />
+            {children}
+        </>
+    );
+
+}
